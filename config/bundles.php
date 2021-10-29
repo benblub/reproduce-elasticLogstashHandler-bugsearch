4cb575a67ace2f86ceefdc641a8ad6685ba3cbaa
benjaminknecht <knecht.benjamin@yahoo.de>
2021-10-29 09:04:30 +0200

    install sf http client and monolog

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
